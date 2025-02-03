f2f15a803ed9c13f09255e26c7504c4a530947a5
Lucas Lima <lucasgabrieljake@gmail.com>
2025-02-03 19:13:52 -0300

    fix: test suites problems working

--- back-end/src/auth/auth.controller.spec.ts
+++ back-end/src/auth/auth.controller.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AtStrategy } from './strategies';
+import { AtStrategy, RtStrategy } from './strategies';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './guards';
 import { RolesGuard } from './guards/roles.guard';
 import { mock } from 'jest-mock-extended';
 import { ConfigModule } from '@nestjs/config';
+import { UserService } from '../user/user.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+
   const service = mock<AuthService>();
+  const usersServiceMock = mock<UserService>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,9 +21,11 @@ describe('AuthController', () => {
       controllers: [AuthController],
       providers: [
         AtStrategy,
+        RtStrategy,
         { provide: APP_GUARD, useClass: AtGuard },
         { provide: APP_GUARD, useClass: RolesGuard },
         { provide: AuthService, useValue: service },
+        { provide: UserService, useValue: usersServiceMock },
       ],
     }).compile();
 
