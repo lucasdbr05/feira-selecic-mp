e9a14de6270e967c2c9528beecac0c344a3bd7d3
Lucas Lima <lucasgabrieljake@gmail.com>
2025-02-04 23:16:56 -0300

    feat: api client

---
+++ front-end/api-client/wrapper.ts
@@ -0,0 +1,122 @@
+import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+type AxiosRequestConfigWithHideToast = AxiosRequestConfig & {
+  hideToast?: boolean;
+};
+
+export interface ApiConfiguration {
+  baseURL?: string;
+  withCredentials: boolean;
+}
+
+export interface IApiClient {
+  post<TRequest, TResponse>(
+    path: string,
+    object: TRequest,
+    config?: AxiosRequestConfigWithHideToast
+  ): Promise<TResponse>;
+  patch<TRequest, TResponse>(
+    path: string,
+    object: TRequest
+  ): Promise<TResponse>;
+  put<TRequest, TResponse>(path: string, object: TRequest): Promise<TResponse>;
+  get<TResponse>(path: string): Promise<TResponse>;
+}
+
+export default class ApiClient implements IApiClient {
+  private client: AxiosInstance;
+
+  protected createAxiosClient(
+    apiConfiguration: ApiConfiguration
+  ): AxiosInstance {
+    return Axios.create(apiConfiguration);
+  }
+
+  constructor(apiConfiguration: ApiConfiguration) {
+    this.client = this.createAxiosClient(apiConfiguration);
+  }
+
+  async post<TRequest, TResponse>(
+    path: string,
+    payload: TRequest,
+    config?: AxiosRequestConfigWithHideToast
+  ): Promise<TResponse> {
+    return await this.client
+      .post<TResponse>(path, payload, config)
+      .then((res) => res.data)
+      .catch((err) => this.handleError(err));
+  }
+
+  async delete<TResponse>(
+    path: string,
+    config?: AxiosRequestConfigWithHideToast
+  ): Promise<TResponse> {
+    return await this.client
+      .delete<TResponse>(path, config)
+      .then((res) => res.data)
+      .catch((err) => this.handleError(err));
+  }
+
+  async patch<TRequest, TResponse>(
+    path: string,
+    payload?: TRequest,
+  ): Promise<TResponse> {
+    return await this.client
+      .patch<TResponse>(path, payload)
+      .then((res) => res.data)
+      .catch((err) => this.handleError(err));
+  }
+
+  async put<TRequest, TResponse>(
+    path: string,
+    payload: TRequest,
+  ): Promise<TResponse> {
+    return await this.client
+      .put<TResponse>(path, payload)
+      .then((res) => res.data)
+      .catch((err) => this.handleError(err));
+  }
+
+  async get<TResponse>(
+    path: string,
+    config?: AxiosRequestConfigWithHideToast
+  ): Promise<TResponse> {
+    return await this.client
+      .get<TResponse>(path, config)
+      .then((res) => res.data)
+      .catch((err) => this.handleError(err));
+  }
+
+  private handleError(error: any): never {
+    this.extractErrorMessage(error);
+    throw error;
+  }
+
+  private extractErrorMessage(error: any): string {
+    const data = error?.response?.data;
+    const firstError = data?.errors?.[0];
+    if (firstError) {
+      if (typeof firstError.description === 'string')
+        return firstError.description; // probably an Asaas error
+
+      if (firstError.property && firstError.errors?.[0])
+        return `${firstError.property}: ${firstError.errors?.[0]}`;
+    }
+
+    if (typeof data?.message === 'string') return data?.message;
+
+    if (typeof data?.message?.[0] === 'string') return data?.message?.[0];
+
+    if (typeof data?.error === 'string') return data?.error;
+
+    return 'Algo deu errado';
+  }
+}
+
+/**
+ * Axios instance of the API, already configured with basic setup
+ */
+export const apiClient = new ApiClient({
+  baseURL: import.meta.,
+  withCredentials: true, // enables cookies
+});
