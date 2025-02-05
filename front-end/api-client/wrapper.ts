aedec5eb50345b9fe0d13c4c34eaaca3dff320a3
Lucas Lima <lucasgabrieljake@gmail.com>
2025-02-04 23:24:05 -0300

    feat: client wrapper

--- front-end/api-client/wrapper.ts
+++ front-end/api-client/wrapper.ts
@@ -1,47 +1,16 @@
-import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { AxiosRequestConfig } from "axios";
+import api from "./client";
 
-type AxiosRequestConfigWithHideToast = AxiosRequestConfig & {
-  hideToast?: boolean;
-};
-
-export interface ApiConfiguration {
-  baseURL?: string;
-  withCredentials: boolean;
-}
-
-export interface IApiClient {
-  post<TRequest, TResponse>(
-    path: string,
-    object: TRequest,
-    config?: AxiosRequestConfigWithHideToast
-  ): Promise<TResponse>;
-  patch<TRequest, TResponse>(
-    path: string,
-    object: TRequest
-  ): Promise<TResponse>;
-  put<TRequest, TResponse>(path: string, object: TRequest): Promise<TResponse>;
-  get<TResponse>(path: string): Promise<TResponse>;
-}
-
-export default class ApiClient implements IApiClient {
-  private client: AxiosInstance;
-
-  protected createAxiosClient(
-    apiConfiguration: ApiConfiguration
-  ): AxiosInstance {
-    return Axios.create(apiConfiguration);
-  }
-
-  constructor(apiConfiguration: ApiConfiguration) {
-    this.client = this.createAxiosClient(apiConfiguration);
-  }
+export class ApiClient {
+  
+  constructor () {}
 
   async post<TRequest, TResponse>(
     path: string,
     payload: TRequest,
-    config?: AxiosRequestConfigWithHideToast
+    config?: AxiosRequestConfig
   ): Promise<TResponse> {
-    return await this.client
+    return await api
       .post<TResponse>(path, payload, config)
       .then((res) => res.data)
       .catch((err) => this.handleError(err));
@@ -49,9 +18,9 @@ export default class ApiClient implements IApiClient {
 
   async delete<TResponse>(
     path: string,
-    config?: AxiosRequestConfigWithHideToast
+    config?: AxiosRequestConfig
   ): Promise<TResponse> {
-    return await this.client
+    return await api
       .delete<TResponse>(path, config)
       .then((res) => res.data)
       .catch((err) => this.handleError(err));
@@ -61,7 +30,7 @@ export default class ApiClient implements IApiClient {
     path: string,
     payload?: TRequest,
   ): Promise<TResponse> {
-    return await this.client
+    return await api
       .patch<TResponse>(path, payload)
       .then((res) => res.data)
       .catch((err) => this.handleError(err));
@@ -71,7 +40,7 @@ export default class ApiClient implements IApiClient {
     path: string,
     payload: TRequest,
   ): Promise<TResponse> {
-    return await this.client
+    return await api
       .put<TResponse>(path, payload)
       .then((res) => res.data)
       .catch((err) => this.handleError(err));
@@ -79,44 +48,16 @@ export default class ApiClient implements IApiClient {
 
   async get<TResponse>(
     path: string,
-    config?: AxiosRequestConfigWithHideToast
+    config?: AxiosRequestConfig
   ): Promise<TResponse> {
-    return await this.client
+    return await api
       .get<TResponse>(path, config)
       .then((res) => res.data)
       .catch((err) => this.handleError(err));
   }
 
-  private handleError(error: any): never {
-    this.extractErrorMessage(error);
+  
+  private handleError(error): never {
     throw error;
   }
-
-  private extractErrorMessage(error: any): string {
-    const data = error?.response?.data;
-    const firstError = data?.errors?.[0];
-    if (firstError) {
-      if (typeof firstError.description === 'string')
-        return firstError.description; // probably an Asaas error
-
-      if (firstError.property && firstError.errors?.[0])
-        return `${firstError.property}: ${firstError.errors?.[0]}`;
-    }
-
-    if (typeof data?.message === 'string') return data?.message;
-
-    if (typeof data?.message?.[0] === 'string') return data?.message?.[0];
-
-    if (typeof data?.error === 'string') return data?.error;
-
-    return 'Algo deu errado';
-  }
 }
-
-/**
- * Axios instance of the API, already configured with basic setup
- */
-export const apiClient = new ApiClient({
-  baseURL: import.meta.,
-  withCredentials: true, // enables cookies
-});
